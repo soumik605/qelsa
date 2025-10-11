f34d66764a688c538dffae615057ea40df824619
Soumik Maity <soumik.maity@geogo.in>
2025-10-12 00:25:48 +0530

    updated

--- src/pages/index.js
+++ src/pages/index.js
@@ -27,7 +27,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <UserSummary />
+        <Default />
       </div>
     </>
   );
